5cfc9ee668fc8321dcc155d3c545b8f0e6adfd31
Miyu-N <myu.star1217@gmail.com>
2021-05-16 22:33:18 +0900

    3章　入力ダイアログ

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-var alertString;
-alertString = addString("WebCamp");
-
-// 作成した関数を呼び出す
-alert(alertString);
-
-// 作成した関数
-function addString(strA){
-  var addStr = "Hello " + strA;
-  return addStr;
-}
-
+var promptStr = prompt('何か好きな文字を入力してください。');
+alert(promptStr);
